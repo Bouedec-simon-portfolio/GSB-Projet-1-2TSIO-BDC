5839873e6b1a98e2fcb68da7f481ac9f15f3f710
Simon-Bouedec <bouedecsimon@gmail.com>
2026-09-23 17:36:28 +0200

    Simplifier le parcours de connexion et le code GSB

--- GSB-CodeIgniter/app/Views/connexion.php
+++ GSB-CodeIgniter/app/Views/connexion.php
@@ -1,5 +1,5 @@
 <?= $this->extend('layout') ?><?= $this->section('content') ?>
-<h1>Connexion</h1><p>Accédez à vos fiches de frais.</p>
+<h1>Connexion</h1><p>Visiteur médical ou administrateur : connectez-vous avec votre compte GSB.</p>
 <form method="post" action="<?= site_url('connexion') ?>" class="card compact"><?= csrf_field() ?>
 <label for="login">Identifiant</label><input id="login" name="login" required maxlength="20" autocomplete="username">
 <label for="password">Mot de passe</label><input id="password" type="password" name="password" required autocomplete="current-password">
